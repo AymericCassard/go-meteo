b60efa594b036cb2b4ff3979d9e60054ca16a387
Aymeric Cassard <aymeric.cassard@sensorfactory.eu>
2024-04-04 10:57:18 +0200

    Add Forecast api

--- controller/request_handler.go
+++ controller/request_handler.go
@@ -32,17 +32,67 @@ func api_getMatchingVilles(sent string) (VillesReponses, error) {
 	return villeResponse, nil
 }
 
+type HourlyTemps struct {
+	Latitude             float64 `json:"latitude"`
+	Longitude            float64 `json:"longitude"`
+	GenerationtimeMs     float64 `json:"generationtime_ms"`
+	UtcOffsetSeconds     int     `json:"utc_offset_seconds"`
+	Timezone             string  `json:"timezone"`
+	TimezoneAbbreviation string  `json:"timezone_abbreviation"`
+	Elevation            float64 `json:"elevation"`
+	HourlyUnits          struct {
+		Time          string `json:"time"`
+		Temperature2M string `json:"temperature_2m"`
+	} `json:"hourly_units"`
+	Hourly struct {
+		Time          []string  `json:"time"`
+		Temperature2M []float64 `json:"temperature_2m"`
+	} `json:"hourly"`
+}
+
+func api_getVillesTemps(latitude, longitude string) (HourlyTemps, error) {
+	var hourlyTemps HourlyTemps
+	response, err := http.Get("https://api.open-meteo.com/v1/forecast?latitude=47.4716&longitude=-0.552&hourly=temperature_2m")
+	if err != nil {
+		return hourlyTemps, err
+	}
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return hourlyTemps, err
+	}
+	err = json.Unmarshal(body, &hourlyTemps)
+	return hourlyTemps, nil
+}
+
 func ReturnVilles(w http.ResponseWriter, r *http.Request) {
 	villes, err := api_getMatchingVilles(r.URL.Query().Get("ville"))
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
 	}
 	for _, result := range villes.Results {
-		components.DataOption(
+		components.VilleButton(
 			result.Name,
 			result.Country,
 			fmt.Sprintf("%f", result.Latitude),
 			fmt.Sprintf("%f", result.Longitude),
 		).Render(r.Context(), w)
+		// components.DataOption(
+		// 	result.Name,
+		// 	result.Country,
+		// 	fmt.Sprintf("%f", result.Latitude),
+		// 	fmt.Sprintf("%f", result.Longitude),
+		// ).Render(r.Context(), w)
+	}
+}
+
+func ReturnHourlyTemps(w http.ResponseWriter, r *http.Request) {
+	temps, err := api_getVillesTemps(r.URL.Query().Get("lat"), r.URL.Query().Get("lon"))
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	for i, result := range temps.Hourly.Time {
+		fmt.Fprintf(w, "Time: %s /", result)
+		fmt.Fprintf(w, " Temp: %f\n", temps.Hourly.Temperature2M[i])
 	}
 }
